b1c26d96b9645ff956906154798fc41318a5234b
keligijus <k.eligijus@gmail.com>
2016-01-22 22:06:55 +0200

    init

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('ngekcv2', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ngMaterial', 'toastr']);
+
+})();
